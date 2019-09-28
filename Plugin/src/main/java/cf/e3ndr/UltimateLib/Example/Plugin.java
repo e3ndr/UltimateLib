745c4247c0ec268720b6a3789f1a720a9efcf545
Ender <33337309+e3ndr@users.noreply.github.com>
2019-09-28 00:08:43 -0500

    Final build for 3
    Example plugin entirely fixed

--- Plugin/src/main/java/cf/e3ndr/UltimateLib/Example/Plugin.java
+++ Plugin/src/main/java/cf/e3ndr/UltimateLib/Example/Plugin.java
@@ -1,5 +1,17 @@
 package cf.e3ndr.UltimateLib.Example;
 
-public class Plugin extends UltimatePlugin {
+import cf.e3ndr.UltimateLib.UltimateLib;
+import cf.e3ndr.UltimateLib.Plugin.UltimatePlugin;
 
+public class Plugin extends UltimatePlugin {
+	@Override
+	protected void pluginEnable(UltimateLib lib) {
+		this.getLogger().println("Hello I\'m the Example plugin wrapped under UltimateLib!");
+		this.registerCommand("", new String[] {"test", "testplugin"}).setExecutor(new CommandTest());
+	}
+	
+	@Override
+	protected void pluginDisable(UltimateLib lib) {
+		this.getLogger().println("Goodbye!");
+	}
 }
