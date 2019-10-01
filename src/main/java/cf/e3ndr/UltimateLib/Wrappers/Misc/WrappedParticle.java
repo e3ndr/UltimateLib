2647836bf9e63b758a9fd6b7e2f16a102aba9528
Ender <33337309+e3ndr@users.noreply.github.com>
2019-09-30 21:36:58 -0500

    Version 11

---
+++ src/main/java/cf/e3ndr/UltimateLib/Wrappers/Misc/WrappedParticle.java
@@ -0,0 +1,30 @@
+/**
+ * Made with <3 by e3ndr.
+ * 
+ * Licensed under MIT, do as you please.
+ */
+package cf.e3ndr.UltimateLib.Wrappers.Misc;
+
+public enum WrappedParticle {
+	BUBBLE,
+    CRITICAL,
+    BLOCK_FORCE_FIELD,
+    SMOKE,
+    EXPLODE,
+    FLAME,
+    LAVA,
+    REDSTONE,
+    HUGE_EXPLODE,
+    HEART,
+    PORTAL,
+    WATER_SPLASH,
+    DRIP_WATER,
+    DRIP_LAVA,
+    FALLING_DUST,
+    SLIME,
+    RAIN_SPLASH,
+    VILLAGER_ANGRY,
+    VILLAGER_HAPPY,
+    ENCHANTMENT_TABLE;
+    
+}
